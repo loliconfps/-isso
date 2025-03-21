8d0c5a8db50e1f23d031d9c3abbc453de082127f
loliconfps <101304990+loliconfps@users.noreply.github.com>
2025-03-21 16:59:15 -0300

    Remover GameBarPresenceWriter

---
+++ Remover GameBarPresenceWriter.bat
@@ -0,0 +1,18 @@
+ÿþ
+@echo off
+
+cls
+title Removedor do GameBarPresenceWriter by zJu4nn
+
+echo Removendo o GameBarPresenceWriter...
+echo.
+
+takeown /f "C:\Windows\System32\GameBarPresenceWriter.exe"
+icacls "C:\Windows\System32\GameBarPresenceWriter.exe" /grant Administrators:F
+del "C:\Windows\System32\GameBarPresenceWriter.exe"
+
+echo.
+echo GameBarPresenceWriter Removido!
+echo.
+pause
+exit
